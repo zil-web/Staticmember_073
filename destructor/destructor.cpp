afbb6a3a7e5c09d36fbf0ef71c8a7b2c281c5eb1
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-17 13:39:10 +0700

    void angka isiData

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -30,3 +30,12 @@ void angka::cetakData() {
 	}
 	cout << endl;
 }
+
+void angka::isiData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = ";cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main(){}
