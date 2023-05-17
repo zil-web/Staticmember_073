2b16ab67ef38c9970d31c9ac99871451aac91dbf
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-17 13:36:00 +0700

    cetak data

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -14,4 +14,19 @@ public:
 //Definisi member functon
 angka::angka(int i) { //costructor
 	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() { //destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "alamat array sudah dilepaskan" << endl;
+}
+void angka::cetakData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = ";cin >> arr[i];
+	}
+	cout << endl;
 }
